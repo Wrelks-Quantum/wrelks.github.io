337560fb3b9041cc2edb71774f35fe3f59748724
Wrelks <49662055+Wrelks@users.noreply.github.com>
2020-11-02 19:24:00 -0500

    Update scene.js

--- js/scene.js
+++ js/scene.js
@@ -266,20 +266,20 @@ function createScene(engine, canvas, config) {
     var qubitStateDiracImagePanel = new BABYLON.GUI.StackPanel();
     qubitStateDiracImagePanel.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
     qubitStateDiracImagePanel.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
-    qubitStateDiracImagePanel.height = adaptRatioStr(346); //org 546 - width
-    qubitStateDiracImagePanel.paddingTop = adaptRatioStr(5); //org 10
+    qubitStateDiracImagePanel.height = adaptRatioStr(546); //org 546 - width
+    qubitStateDiracImagePanel.paddingTop = adaptRatioStr(10); //org 10
     advancedTexture.addControl(qubitStateDiracImagePanel);
 
     var qubitStateDiracImage = new BABYLON.GUI.Image("but", "images/qubit-state-dirac.png");
-    qubitStateDiracImage.width = adaptRatioStr(346); //change the size of the image, org 546 - width
-    qubitStateDiracImage.height = adaptRatioStr(24); //change the size of the image, org 48
+    qubitStateDiracImage.width = adaptRatioStr(546); //change the size of the image, org 546 - width
+    qubitStateDiracImage.height = adaptRatioStr(48); //change the size of the image, org 48
     qubitStateDiracImagePanel.addControl(qubitStateDiracImage);
 
     var qubitStateDiracTextPanel = new BABYLON.GUI.StackPanel();
     qubitStateDiracTextPanel.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
     qubitStateDiracTextPanel.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
-    qubitStateDiracTextPanel.height = adaptRatioStr(346); //org 546 - width
-    qubitStateDiracTextPanel.paddingTop = adaptRatioStr(5); //org 10
+    qubitStateDiracTextPanel.height = adaptRatioStr(546); //org 546 - width
+    qubitStateDiracTextPanel.paddingTop = adaptRatioStr(10); //org 10
     advancedTexture.addControl(qubitStateDiracTextPanel);
 
     var qubitStateDiracTextBlock = new BABYLON.GUI.TextBlock();
